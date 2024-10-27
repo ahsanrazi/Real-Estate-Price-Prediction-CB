880edfd7d961070eb8f56a40ae440ff9e2daf312
Ahsan Raza <ahsanraza8767@gmail.com>
2024-10-27 01:35:26 -0700

    update files

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 
 app = Flask(__name__)
+CORS(app)
 
 # Routine that return all the locations from json file
 @app.route('/get_location_names', methods=['GET'])
